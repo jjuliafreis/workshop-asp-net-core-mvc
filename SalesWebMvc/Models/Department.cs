689348c332085088a1bb6a631003a6206c15daf1
Julia Reis <jubsr4199@gmail.com>
2026-02-24 12:00:12 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Permissions;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
